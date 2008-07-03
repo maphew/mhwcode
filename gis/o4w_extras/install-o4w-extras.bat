1a15b5e591f866d0465429cc48ac17b7fb636f36
maphew <maphew@localhost>
2008-07-03 22:33:24 +0000

    switched to using xcopy instead, enables easy ignoring of .svn files (and is easier to read the batchfile too)

--- gis/o4w_extras/install-o4w-extras.bat
+++ gis/o4w_extras/install-o4w-extras.bat
@@ -5,37 +5,31 @@ echo.
 
 :: Environment settings
 if [%OSGEO4W_ROOT%]==[] goto :EnvNotSet
-setlocal enabledelayedexpansion
-:: Capture drive letter and parent dir tree of where the o4w goodies are sitting
-set _prefix=%~dp0
 
-:Main
-for /r %%g in (*) do (
-	set srcFile=%%g
-	REM strip parent dir tree,
-	REM turn x:\temp\Osgeo4w.extras\bin\gdalcopyproj.py    into    c:\osgeo4w\bin\gdalcopyproj.py
-	set dstFile=!srcFile:%_prefix%=%OSGEO4W_ROOT%\!
-	
-	if not exist "!dstFile!" echo.	!dstFile!
-	if not exist "!dstFile!" copy "!srcFile!" "!dstFile!"
-	
-	)
+:xcopy
+	cd /d %~dp0
+	if not exist xcopy_exclude.txt echo .svn > xcopy_exclude.txt
+	xcopy /s /exclude:xcopy_exclude.txt /d /y .\* %OSGEO4W_ROOT%\
+	call :MakeBats
+	goto :Done
 
-cd /d %OSGEO4W_ROOT%\bin
-
-for %%g in (*.py) do if not exist %%~ng.bat echo @python "%%OSGEO4W_ROOT%%\bin\%%g" %%* > %%~ng.bat
-
-goto :Done
-
+:MakeBats
+	cd /d %OSGEO4W_ROOT%\bin
+	for %%g in (*.py) do (
+		if not exist %%~ng.bat echo @python "%%OSGEO4W_ROOT%%\bin\%%g" %%* > %%~ng.bat
+		)
+	goto :EOF
 
 :EnvNotSet
-echo.
-echo.	*** OSGEO4W_ROOT not found. 
-echo.	*** Please run install-o4w-extras.bat from within OSGeo4W shell.
-echo.
-:: pause for a few seconds.
-ping localhost -n 5 >nul
-goto :EOF	
+	echo.
+	echo.	*** OSGEO4W_ROOT not found. 
+	echo.	*** Please run install-o4w-extras.bat from within OSGeo4W shell.
+	echo.
+	:: pause for a few seconds.
+	ping localhost -n 5 >nul
+	goto :EOF	
 
 :Done
-echo.
+	echo.
+	echo.	Finished.
+	echo.
