31bbd8dc7cd2df835539b4dd223bfcc5bf05ee50
maphew <maphew@localhost>
2009-10-22 07:35:56 +0000

    thanks to walk-registry.py, finally have the beginnings of a function to list known python installs. Overall script is still a train wreck though.

--- gis/o4w_extras/register-python.py
+++ gis/o4w_extras/register-python.py
@@ -23,11 +23,61 @@ pythonpath = "%s;%s\\Lib\\;%s\\DLLs\\" % (
     installpath, installpath, installpath
 )
 
-# http://sebsauvage.net/python/snyppets/#registry
-key = OpenKey(HKEY_CURRENT_USER, regpath, 0, KEY_READ)
-(value, valuetype) = QueryValueEx(key, installkey)
-print value
-print valuetype
+#FIXME: check for HKCU Python before going on to HKLM
+#Q: do we register for current user or all users? (HKCU or HKLM?)
+#FIXME: handle 64bit machines, http://www.python-forum.org/pythonforum/viewtopic.php?f=15&t=12685
+
+#TODO: http://timgolden.me.uk/python-on-windows/programming-areas/registry.html
+regpath = r"software\python\pythoncore"
+key = OpenKey(HKEY_CURRENT_USER, regpath)
+subkeys = []
+i = 0
+while True:
+    try:
+        subkeys.append (EnumKey (key, i))
+        i += 1
+        print subkeys
+    except EnvironmentError:
+        break
+
+# http://effbot.org/librarybook/winreg.htm
+# this lists values within keys, but skips keys within keys
+def ExploreReg():
+    explorer = OpenKey(
+    HKEY_CURRENT_USER,
+    regpath    
+    )
+    # list values owned by this registry key
+    try:
+        i = 0
+        while 1:
+            name, value, type = EnumValue(explorer, i)
+            print repr(name),
+            i += 1
+    except WindowsError:
+        print
+    #
+    #value, type = QueryValueEx(explorer, "Logon User Name")
+    #
+    #print
+    #print "user is", repr(value)
+    try:
+        i = 0
+        while 1:
+            name, value, type = EnumKey(regpath, i)
+            print repr(name),
+            i += 1
+    except WindowsError:
+        print
+
+ExploreReg()
+
+## http://sebsauvage.net/python/snyppets/#registry
+#key = OpenKey(HKEY_CURRENT_USER, 'Software\\Microsoft\\Internet Explorer', 0, KEY_READ)
+#key2 = 'Desktop'
+#(value, valuetype) = QueryValueEx(key, key2)
+#print value
+#print valuetype
 
 
 def RegisterPy():
@@ -56,5 +106,6 @@ def RegisterPy():
     print "*** You probably have another Python installation!"
 
 if __name__ == "__main__":
-    RegisterPy()
+    #RegisterPy()
+    pass
 
