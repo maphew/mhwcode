8d689bba6810a5e68389c84916914eb91745dffe
maphew <maphew@localhost>
2010-03-04 07:51:21 +0000

    reg-py refactor: make easier to read, document x64 bug.

--- gis/o4w_extras/register-python.py
+++ gis/o4w_extras/register-python.py
@@ -8,40 +8,43 @@
 # modified by Matt Wilkie for OSGeo4W
 #
 # Adapted from:
-#	http://www.pythonware.com/products/works/articles/regpy20.htm
-#	http://effbot.org/zone/python-register.htm
-#	http://timgolden.me.uk/python-on-windows/programming-areas/registry.html
-
+#   http://www.pythonware.com/products/works/articles/regpy20.htm
+#   http://effbot.org/zone/python-register.htm
+#   http://timgolden.me.uk/python-on-windows/programming-areas/registry.html
+#
+# Known problems:
+#   Doesn't detect existing python registrations on 64bit machines,
+#   see http://www.mail-archive.com/python-list@python.org/msg266397.html
+#
 import sys
 from _winreg import *
 
 # grab some details of python environment running this script
 # tweak as necessary
 our_version = sys.version[:3]
-installpath = sys.prefix
+our_installpath = sys.prefix
 
-regpath = "SOFTWARE\\Python\\Pythoncore\\"
+pycore_regpath = "SOFTWARE\\Python\\Pythoncore\\"
 installkey = "InstallPath"
 pythonkey = "PythonPath"
-pythonpath = "%s;%s\\Lib\\;%s\\DLLs\\" % (
-    installpath, installpath, installpath
-)
+pythonpath = "%s;%s\\Lib\\;%s\\DLLs\\" % \
+        (our_installpath, our_installpath, our_installpath)
 
-def get_existing(hkey,regpath):
-    ''' retrieve all existing python registrations '''
+def get_existing(hkey, pycore_regpath):
+    ''' retrieve existing python registrations '''
     #TODO: retrieve install path, to know what program existing reg belong to
     if hkey == 'Current':
-		try:
-			key = OpenKey(HKEY_CURRENT_USER, regpath)
-		except WindowsError:
-			#print WindowsError()
-			return
+        try:
+            key = OpenKey(HKEY_CURRENT_USER, pycore_regpath)
+        except WindowsError:
+            #print WindowsError()
+            return
     elif hkey == 'All':
-		try:
-			key = OpenKey(HKEY_LOCAL_MACHINE, regpath)
-		except WindowsError:
-			#print WindowsError()
-			return
+        try:
+            key = OpenKey(HKEY_LOCAL_MACHINE, pycore_regpath)
+        except WindowsError:
+            #print WindowsError()
+            return
 
     subkeys = []
     i = 0
@@ -54,24 +57,25 @@ def get_existing(hkey,regpath):
             break
     return subkeys
 
-def RegisterPy(pycorepath, version):
-    regpath = pycorepath + version
+def RegisterPy(pycore_regpath, version):
+    ''' put this python install into registry '''
+    pycore_regpath = pycore_regpath + version
     try:
-        reg = OpenKey(HKEY_LOCAL_MACHINE, regpath)
+        reg = OpenKey(HKEY_LOCAL_MACHINE, pycore_regpath)
         regVal = QueryValueEx(reg, installkey)[0]
         print regVal
     except EnvironmentError:
         try:
-            reg = CreateKey(HKEY_LOCAL_MACHINE, regpath)
-            SetValue(reg, installkey, REG_SZ, installpath)
+            reg = CreateKey(HKEY_LOCAL_MACHINE, pycore_regpath)
+            SetValue(reg, installkey, REG_SZ, our_installpath)
             SetValue(reg, pythonkey, REG_SZ, pythonpath)
             CloseKey(reg)
         except:
             print "*** Unable to register!"
             return
-        print "--- Python %s is now registered to %s!" % (our_version, installpath)
+        print "--- Python %s is now registered to %s!" % (our_version, our_installpath)
         return
-    if (QueryValue(reg, installkey) == installpath and
+    if (QueryValue(reg, installkey) == our_installpath and
         QueryValue(reg, pythonkey) == pythonpath):
         CloseKey(reg)
         print "=== Python %s is already registered!" % (our_version)
@@ -80,26 +84,25 @@ def RegisterPy(pycorepath, version):
     print "*** Unable to register!"
     print "*** You probably have another Python installation!"
 
-
 #---
-CurrentUser = get_existing('Current',regpath)
-AllUsers = get_existing('All',regpath)
+CurrentUser = get_existing('Current',pycore_regpath)
+AllUsers = get_existing('All',pycore_regpath)
 print '''
-	Existing Current User python version(s):  %s
-	Existing All Users python version(s):\t  %s
+    Existing Current User python version(s):  %s
+    Existing All Users python version(s):\t  %s
 ''' % (CurrentUser, AllUsers)
 
 if CurrentUser:
-	match = True if our_version in CurrentUser else False
+    match = True if our_version in CurrentUser else False
 elif AllUsers:
-	match = True if our_version in AllUsers else False
+    match = True if our_version in AllUsers else False
 else:
-	RegisterPy(regpath,our_version)
+    RegisterPy(pycore_regpath,our_version)
 
 try:
-	if match:
-		print 'Our version (%s) already registered. Skipping...' % (our_version)
+    if match:
+        print 'Our version (%s) already registered. Skipping...' % (our_version)
 except:
-	pass
+    pass
 
 #-- the end
