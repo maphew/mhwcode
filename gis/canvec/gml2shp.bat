71c94239902ab968614bbc7f1e216df2e5ccb909
maphew <maphew@localhost>
2010-04-28 22:56:22 +0000

    gml2shp.bat * try to make it easier to NOT project to yukon albers. Moved heavy lifting commands to :labels because the if...else kept evaluating wrong. Seems CMD doesn't like multi line commands inside a conditional.

--- gis/canvec/gml2shp.bat
+++ gis/canvec/gml2shp.bat
@@ -3,38 +3,49 @@
 ::
 if "%2" =="" goto :Usage
 
-set srcDir=%1
-set outDir=%2
+:: Handle command line arguments,
+:: kudos to Patrick Cuff - http://stackoverflow.com/questions/382587
+for /f "tokens=1-3*" %%a in ("%*") do (
+    set srcDir=%1
+    set outDir=%2
+    set arg3=%%c
+    set argTheRest=%%d
+)
+
 set ogrArgs= -f "esri shapefile"
 
 :: OGR parameters to project from NAD83CSRS to Yukon Albers
 set yt_alb="+proj=aea +lat_1=61.66666666666666 +lat_2=68.0 +lat_0=59.0 +lon_0=-132.5 +x_0=500000 +y_0=500000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
-if "%3"=="yukon" (
+
+if "%arg3%"=="yukon" (
    set ogrArgs=%ogrArgs% -s_srs epsg:4617 -t_srs %yt_alb% ) else (
-   set ogrArgs=%ogrAtrgs% %3
+   set ogrArgs=%ogrArgs% %argTheRest%
    )
 
 call :Main
 goto :EOF
 
 :Main
-	pushd %srcDir%
-	for %%f in (*.gml) do (
-   	echo Processing %%f
+    pushd %srcDir%
+    for %%f in (*.gml) do (
+        echo Processing %%f
+        REM convert from GML to SHP and merge
+        if not exist [%outDir%] (call :New %%f) else (call :Append %%f)
+       )
+    popd
+    goto :EOF
 
-   	REM convert from GML to SHP and merge
-   	if not exist %outDir% (
-      	rem echo ogr2ogr %ogrArgs% %outDir% %%f
-      	ogr2ogr %ogrArgs% -append %outDir% %%f) else (
-         	rem echo ogr2ogr %ogrArgs% -update -append %outDir% %%f
-         	ogr2ogr %ogrArgs% -update -append %outDir% %%f
-            )
-      rem pass
-   )
-	popd
-	goto :EOF
+:New
+    echo ogr2ogr %ogrArgs% %outDir% %1
+    ogr2ogr %ogrArgs% -append %outDir% %1
+    goto :eof
+
+:Append
+    echo ogr2ogr %ogrArgs% -update -append %outDir% %1
+    ogr2ogr %ogrArgs% -update -append %outDir% %1
+    goto :eof
 
 :Usage
-	echo.
-	echo. Usage: %~n0 [path\to\*.gml] [output dir] {ogr arguments}
-	echo.
+    echo.
+    echo. Usage: %~n0 [path\to\*.gml] [output dir] {ogr arguments}
+    echo.
