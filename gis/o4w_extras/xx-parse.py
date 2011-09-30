6fd904975d0d7d4699d97206013a7bb36e724316
maphew <maphew@localhost>
2011-09-29 23:55:43 -0700

    register-python.py: clean up messages. Now believed to work reliably on 32bit windows (needs testing).

--- gis/o4w_extras/xx-parse.py
+++ gis/o4w_extras/xx-parse.py
@@ -8,11 +8,8 @@
 #@@tabwidth -4
 #@+others
 #@+node:maphew.20110914213235.1221: ** parse command line
-__version__ = '2011-09-14.20:47'
-# print '-={ %s }=-\n'% (str.strip(svn_id, ' $'))    
 #@verbatim
 #@url http://www.doughellmann.com/PyMOTW/argparse/
-
 import argparse, sys
 
 # @url http://stackoverflow.com/questions/4042452/display-help-message-with-python-argparse-when-script-is-called-without-any-argum
