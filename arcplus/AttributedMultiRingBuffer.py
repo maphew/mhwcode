2677a209d9fcb3443fcd63b7e420e7dea5b870a7
maphew <maphew@localhost>
2012-02-02 12:04:58 -0800

    AttributedMultiRingBuffer.py: convert to use commandline parameters, add usage notes, enable removal of interim files

--- arcplus/AttributedMultiRingBuffer.py
+++ arcplus/AttributedMultiRingBuffer.py
@@ -1,11 +1,19 @@
 #@+leo-ver=5-thin
-#@+node:maphew.20120201131727.1848: * @file multi-ring-buffer.py
+#@+node:maphew.20120201135209.1853: * @file arcplus/AttributedMultiRingBuffer.py
 ''' multi-ring-buffer.py: Create multiple ring buffers, keeping named attributes of parent feature class.
 
 Process:
     - create inside only buffer for each of the specifed buffer widths
     - store buffer width used as an attribute
     - merge all buffers into a single feature class, ensuring largest width first so narrower ones are drawn on top
+
+Usage:
+    multi-ring-buffer [feature class]  [workspace]  [output feature class]  [widths list]  [attributes to keep]
+    
+    multi-ring-buffer R:\data.gdb\Foobar_ply  X:\maps\buffers.gdb  Foobar_rings   50,-50,-100,-300,-600  NAME,TYPE
+   
+Workspace must exist.
+The width and attribute parameters must be comma separated and have no spaces.
 
 Requires Arcgis 10, Arcinfo license level.
 
@@ -18,21 +26,23 @@ Also see http://gis.stackexchange.com/questions/19505/multiple-ring-buffer-with-
 import arcpy
 from arcpy import env
 
-in_fc = r'R:\v5\ENV_1000k.gdb\admin_other\FNTT' # features to buffer
-out_fc = 'FNTT_ringbuffers'                                 # finished result
-distances = ['-1000','-3000','-5000']                      # buffer widths, add as desired
-wspace = 'D:/s/default.gdb/'
+in_fc = arcpy.GetParameterAsText(0)                   # features to buffer
+wspace = arcpy.GetParameterAsText(1)                # output workspace
+out_fc = arcpy.GetParameterAsText(2)                 # finished result
+distances = arcpy.GetParameterAsText(3).split(',')  # list of buffer widths 
+# parse list of attributes to keep into semi-colon separated as expected by buffer tool
+dissolveFields = arcpy.GetParameterAsText(4).replace(',',';')
 
 env.workspace = wspace
 
 sideType = "OUTSIDE_ONLY"
 endType = "ROUND"
 dissolveType = "LIST"
-dissolveFields = "NAME;TYPE"    # attributes we want to keep, adjust as needed 
 
 buffered_fcs = []
 for distance in distances:
     buf_fc = arcpy.ValidateTableName('xxx_' + out_fc + distance)
+    print("...buffering {0} into {1} with width {2}".format(in_fc, buf_fc, distance))
     
     arcpy.Buffer_analysis(in_fc, buf_fc, distance, sideType, endType, dissolveType, dissolveFields)
     
@@ -47,9 +57,11 @@ for distance in distances:
 buffered_fcs.sort()
 buffered_fcs.reverse() 
 
+print("...Merging intermediate buffers into {0}".format(out_fc))
 arcpy.Merge_management(buffered_fcs, out_fc)
 
-## remove temporary intermediate files
-#for fc in arcpy.ListFeatureClasses('xxx_*')
-#    arcpy.Delete_management(fc)
+# remove temporary intermediate files
+print("...Removing intermediate files")
+for fc in arcpy.ListFeatureClasses('xxx_*'):
+    arcpy.Delete_management(fc)
 #@-leo
