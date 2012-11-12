4d3bcfccfb46ecd1bdb002b4f9d2dff8edf4287b
maphew <maphew@localhost>
2012-11-11 22:31:48 -0800

    a2: some changes with mirror that I don't recall details of; internet was down and didn't have hg available.

--- apt/a2.py
+++ apt/a2.py
@@ -1,5 +1,3 @@
-#@+leo-ver=5-thin
-#@+node:maphew.20120623001354.1529: * @file a2.py
 ''' 2nd attempt at using Plac module to drive Apt.
     Adapted from the 'Fake version control system' example:
     http://plac.googlecode.com/hg/doc/plac.html#a-non-class-based-example '''
@@ -26,15 +24,16 @@ apt.download_p = 0  # download only flag, 1=yes
 apt.depend_p = 0  # ignore dependencies flag, 1=ignore
 apt.installed = 0
 
-try:
-    apt.mirror = apt.get_config('last-mirror')  # the url
-    apt.mirror_dir = urllib.quote (apt.mirror, '').lower () # the local filesystem dir name
-    apt.last_cache = apt.get_config('last-cache')
-    apt.downloads = '%s/%s' % (apt.last_cache, apt.mirror_dir)
-    apt.get_setup_ini() # parse setup.ini into package name, version, etc.
-    apt.get_installed()
-except:
-    print '-' *10, 'Error reading last-mirror, last-cache, downloads dir, installed packages'
+apt.last_mirror = apt.get_config('last-mirror')
+apt.mirror = apt.get_mirror()  # the url
+apt.mirror_dir = urllib.quote (apt.mirror, '').lower () # the local filesystem dir name
+apt.last_cache = apt.get_config('last-cache')
+apt.downloads = '%s/%s' % (apt.last_cache, apt.mirror_dir)
+apt.get_setup_ini() # parse setup.ini into package name, version, etc.
+apt.get_installed()
+# try:
+# except:
+    # print '-' *10, 'Error reading last-mirror, last-cache, downloads dir, installed packages'
         
 @plac.annotations(packages='package(s) to operate on')
 def install(*packages):
@@ -55,10 +54,8 @@ def remove(*packages):
 @plac.annotations(mirror=('alternate url to use', 'option'))
 def update(mirror):
     "download latest packages info from mirror"    
-    print '-' *10 + ' running apt update', mirror
-   
+    print '-' *10 + ' running apt update', mirror   
     apt.update()
-    
     return (10*'-'+ 'Update finished ', '')
 
 @plac.annotations(quiet=('summary information', 'flag', 'q'))
@@ -82,4 +79,3 @@ if __name__=='__main__':
     import plac
     for out in plac.call(main): print(out)
 
-#@-leo
