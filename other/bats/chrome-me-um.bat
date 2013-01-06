3cc02f45041810e57c0f70746776ca6016a1b0f2
Matt <Matt@server.yt.northwestel.net>
2013-01-06 11:23:47 -0800

    bugfix: look for chrome.exe in other places beside user profile (thanks for Nemanja for reporting the bug)

--- other/bats/chrome-me-um.bat
+++ other/bats/chrome-me-um.bat
@@ -1,23 +1,40 @@
 @echo off
 :: Chrome-me-um - start Chrome browser with speficified user profile
 :: Matt Wilkie, 2008-sept-26.
+::  2013-Jan-06 + add FindChromeExe
 :: this script is public domain
 if [%1]==[] goto :Usage
 
 setlocal
 set _user=%1
 set _root=%homedrive%%homepath%\Local Settings\Application Data\Google\Chrome
-set _chrome="%_root%\Application\chrome.exe"
 set _profile="%_root%\User Data\%_user%"
 if not exist %_profile% set _opt=-first-run
+call :FindChromeExe
 
-start /b "%0%" %_chrome% --user-data-dir=%_profile% %_opt%
+if defined _chrome (
+    start /b "%0" %_chrome% --user-data-dir=%_profile% %_opt%)
 
-goto :EOF
+goto :eof
+rem ---------- End ---------- 
+
+:FindChromeExe
+    if exist "%_root%\Application\chrome.exe" (
+        set _chrome="%_root%\Application\chrome.exe")
+
+    if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
+        set _chrome="%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe")
+
+    if exist "%ProgramFiles%Google\Chrome\Application\chrome.exe" (
+        set _chrome="%ProgramFiles%Google\Chrome\Application\chrome.exe")
+
+    if not defined _chrome echo. *** Error, couldn't locate chrome.exe
+    goto :eof
 
 :Usage
-echo.
-echo. Start Google Chrome browser with a profile. Example:
-echo.
-echo.    %~n0 Noah_Body
-echo.
+    echo.
+    echo. Start Google Chrome browser with a profile. Example:
+    echo.
+    echo.    %~n0 Noah_Body
+    echo.
+    goto :eof
