3cedd80daa612295ce92522601aacd97fd84fd20
Matt Wilkie <maphew@gmail.com>
2015-01-28 10:28:30 -0800

    SpatCoord string needs to be single-quoted
    ...assuming input is using double-quotes, which is frequent but not guaranteed.
    Really sys.argv[1] needs to be quote tested and re-wrapped (and might as well
    read from file while we're at it)

--- gis/o4w_extras/bin/proj2wkt.py
+++ gis/o4w_extras/bin/proj2wkt.py
@@ -1,5 +1,5 @@
 '''Convert Well Known Text (WKT) spatial reference to Proj4.
-    
+
     Adapted from http://spatialnotes.blogspot.ca/2010/11/converting-wkt-projection-info-to-proj4.html
 '''
 import os
@@ -8,7 +8,7 @@ import string
 import osgeo.osr
 
 if (len(sys.argv) <> 2):
-        print 'Usage: proj2wkt.py [Proj4 Projection Text]'
+        print "Usage: proj2wkt.py '[Proj4 Projection Text]'"
 else:
         srs = osgeo.osr.SpatialReference()
         srs.ImportFromProj4(sys.argv[1])
