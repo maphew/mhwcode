0ff7ba94f2a68d0455549861cefaf16d175d22ec
matt wilkie <maphew@gmail.com>
2018-03-06 21:46:39 -0800

    Update xxcopy-change-install-date.bat

--- other/xxcopy-change-install-date.bat
+++ other/xxcopy-change-install-date.bat
@@ -1,3 +1,4 @@
+@echo off
 :: The author of XXCOPY is deceased. The last version ever is 3.33.3, 2016-10-28.
 :: There is no possiblity anymore of registering.
 ::
@@ -5,6 +6,7 @@
 :: is this recipe to remove the nag screen that shows up after >60 days since
 :: xxcopy was installed for unregistered versions.
 ::
+
 Echo Windows Registry Editor Version 5.00 >TempReg.reg
 
 Echo [HKEY_LOCAL_MACHINE\SOFTWARE\Pixelab\XXCOPY] >>TempReg.reg
@@ -22,9 +24,14 @@ Echo   00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,5b,5a,5a,5a,5b,5a,5
 Echo   5a,fc,5f,ff,ff,50,8e,6b,07,03,07,d0,01,50,8e,6b,07,03,07,d0,01,5a,5a,5a,5a,\ >>TempReg.reg
 Echo   5a,5a,5a,5a,00,00,00,00,38,a7,05,ec >>TempReg.reg
 
-Regedit /s TempReg.reg
+
+REM Subinacl /keyreg "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\vwintab.exe." /grant=Everyone=F
+REM Subinacl /keyreg "HKEY_LOCAL_MACHINE\SOFTWARE\Pixelab\XXCOPY" /grant=Everyone=S
+
+REM Regedit /s TempReg.reg
 
 Subinacl /keyreg "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\vwintab.exe." /deny=Everyone=F
 Subinacl /keyreg "HKEY_LOCAL_MACHINE\SOFTWARE\Pixelab\XXCOPY" /deny=Everyone=S
 
-Del TempReg.reg
+
+REM Del TempReg.reg
