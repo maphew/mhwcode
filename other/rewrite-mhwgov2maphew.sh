a85f624b7b65d18a3916cd0c10e516e909eeaa63
matt wilkie <maphew@gmail.com>
2019-09-03 21:31:15 -0700

    quick and dirty scripts to rewrite git commit history
    ...for when I've forgotten to set Repo Author settings on a new clone before making commits.

---
+++ other/rewrite-mhwgov2maphew.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Rewrite all commits in this repo attributed for my work address to my personal one.
+# WARNING! is destructive for collaborators. See:
+# https://help.github.com/en/articles/changing-author-info 
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="matt.wilkie@gov.yk.ca"
+CORRECT_NAME="matt wilkie"
+CORRECT_EMAIL="maphew@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
